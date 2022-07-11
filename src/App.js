0be33a66b6f1214de2923722cc39c9b1840f8c7a
luckyclutcher <luckyclutcher@protonmail.com>
2022-07-11 17:36:49 -0400

    commit with the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div id="content">
+    <h1>
+        Portfolio
+    </h1>
+    <h2>Leo MARIE</h2>
+
+    <a className="card" style={{"display": "block"}} target="_blank" href="https://cryptosonos.netlify.app/">
+        Crypto Sonos
+    </a>
+    <p className ="maintext">NFT mini-game on Ethereum Rinkeby test-net.</p>
+    <p className="secondarytext">Smart Contract coded with Solidity.</p>
+    <p className="secondarytext">UI coded with React.</p>
+
+    <a className="card" style={{"display": "block"}} target="_blank" href="https://masterm1nd.netlify.app/">
+        Mastermind
+    </a>
+    <p className ="maintext">Mastermind mini-game.</p>
+    <p className="secondarytext">UI coded with React.</p>
+
+    <a className="card" style={{"display": "block"}} target="_blank" href="https://pu1ssance4.netlify.app/">
+        Connect 4
+    </a>
+    <p className ="maintext">Connect 4 mini-game.</p>
+    <p className="secondarytext">UI coded with React.</p>
+
+    <a className="card" style={{"display": "block"}} target="_blank" href="https://leo-marie.github.io/chifoumi/">
+        Rock Paper Scissors
+    </a>
+    <p className ="maintext">Rock-Paper-Scissors mini-game.</p>
+    <p className="secondarytext">UI coded with React.</p>
+    
+  </div>
+
+  )
+
+} // App
 
 export default App;
